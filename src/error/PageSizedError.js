df2e0573cf4272eda1be8e3af61882ccfea9f3df
wvxbs <gabriel.ferreira7854@gmail.com>
2022-02-08 23:54:53 -0300

    Page sized error updated

--- src/error/PageSizedError.js
+++ src/error/PageSizedError.js
@@ -1,7 +1,7 @@
 const PageSizedError = (props) => {
     return (
         <div className="page-sized-error">
-            <h1 className="page-sized-error-text">
+            <h1 className="page-sized-error-text title is -1">
                 {props.title}
             </h1>
         </div>
