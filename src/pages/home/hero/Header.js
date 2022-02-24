a2b90fa54ce4850b4d6bd6d5059410ae122c01d6
wvxbs <gabriel.ferreira7854@gmail.com>
2022-02-24 20:58:49 -0300

    styling and header updated

--- src/pages/home/hero/Header.js
+++ src/pages/home/hero/Header.js
@@ -21,11 +21,9 @@ const Header = props => {
       <div className="hero-head">
         <div className="navbar is-white">
           <div className="container">
-            <div className="navbar-brand">
+            <div className="navbar-start">
               <div className="navbar-item">
-                <img 
-                  src={props.data.HeaderContent.icon}
-                />
+                {RenderSimpleNavbarItems(props.data.SimpleNavbarItems)}
               </div>
               <a className={`navbar-burguer ${isActive ? "is-active" : ""}`} data-target="navbarMenuHeroC" onclick={HandleClick} aria-label="menu" aria-expanded="false">
                 <div aria-hidden="true"></div>
@@ -35,7 +33,6 @@ const Header = props => {
             </div>
             <div id="navbarMenuHeroC" className={`navbar-menu ${isActive ? "is-active" : ""}`}>
               <div className="navbar-end">
-                {RenderSimpleNavbarItems(props.data.SimpleNavbarItems)}
                 <Button
                   url={props.data.HeaderContent.button.url}
                   text={props.data.HeaderContent.button.text}
